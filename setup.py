dc82032bdcc69fb0fc282d61b4809ef75f5189df
tillahoffmann <t.hoffmann13@imperial.ac.uk>
2016-09-27 15:54:28 +0100

    Make pip installable.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='util',
+    version='0.1',
+    url='https://github.com/tillahoffmann/congress',
+    author='Till Hoffmann',
+    packages=['util']
+)
