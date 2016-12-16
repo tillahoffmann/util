2f87809b3ba282d309ac51990a218510f69e6a6b
tillahoffmann <t.hoffmann13@imperial.ac.uk>
2016-12-16 17:44:43 +0000

    Add linear/square conversion routines.

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,12 +1,46 @@
 import util
 import numpy as np
+import pytest
 
 
-def test_offdiag_indices():
-    i, j = util.offdiag_indices(100)
+@pytest.fixture(params=[True, False])
+def directed(request):
+    return request.param
+
+
+def test_offdiag_indices(directed):
+    i, j = util.offdiag_indices(100, directed)
     assert np.all(i != j), "indices must not be diagonal"
+    if directed:
+        assert len(i) == 100 * 99, "unexpected length"
+    else:
+        assert len(i) == 100 * 99 / 2, "unexpected length"
 
 
-def test_square_size():
-    i, j = util.offdiag_indices(100)
-    assert util.square_size(len(i)) == 100, "unexpected result"
+def test_square_size(directed):
+    i, _ = util.offdiag_indices(100, directed)
+    assert util.square_size(len(i), directed) == 100, "unexpected result"
+
+
+def test_square_array(directed):
+    x = np.random.normal(0, 1, (100 * 99) if directed else (100 * 99 / 2))
+    square = util.square_array(x, directed)
+    if not directed:
+        np.testing.assert_equal(square, square.T, 'square array must be symmetric')
+    assert square.shape == (100, 100), "unexpected shape"
+
+
+def test_linear_array(directed):
+    x = np.random.normal(0, 1, (100, 100))
+    linear = util.linear_array(x, directed)
+    if directed:
+        assert len(linear) == 100 * 99, "unexpected length"
+    else:
+        assert len(linear) == 100 * 99 / 2, "unexpected length"
+
+
+def test_linear_square_linear_array(directed):
+    desired = np.random.normal(0, 1, (100 * 99) if directed else (100 * 99 / 2))
+    square = util.square_array(desired, directed)
+    actual = util.linear_array(square, directed)
+    np.testing.assert_equal(actual, desired, 'could not recover linear array')
--- util/graph.py
+++ util/graph.py
@@ -1,18 +1,93 @@
 import numpy as np
 
 
-def square_size(condensed_size):
+def square_size(linear_size, directed=True):
     """
-    Compute the size of a square matrix from a condensed matrix without diagonal elements
-    (size `num_nodes * num_nodes`).
+    Compute the size of a square matrix from a condensed matrix without diagonal elements.
+
+    Parameters
+    ----------
+    linear_size : int
+        size of a condensed matrix
+    directed : bool
+        whether the condensed matrix is directed
+
+    Returns
+    -------
+    square_size : int
+        size of the corresponding square matrix
     """
-    x = 0.5 * (1 + np.sqrt(1 + 4 * condensed_size))
+    if not directed:
+        linear_size *= 2
+    x = 0.5 * (1 + np.sqrt(1 + 4 * linear_size))
     assert x == int(x), "expected an integer but got %f" % x
     return int(x)
 
 
-def offdiag_indices(n):
+def offdiag_indices(n, directed=True):
     """
     Return the off diagonal indices of a `n` by `n` matrix.
+
+    Parameters
+    ----------
+    n : int
+        size of the square matrix
+    directed : bool
+        whether the condensed matrix is directed
+
+    Returns
+    -------
+    i : np.ndarray
+        offdiagonal index along the first dimension
+    j : np.ndarray
+        offdiagonal index along the second dimension
     """
-    return np.nonzero(1 - np.eye(n))
+    if directed:
+        return np.nonzero(1 - np.eye(n))
+    else:
+        return np.triu_indices(n, 1)
+
+
+def square_array(linear, directed=True):
+    """
+    Convert a linear vector to a square matrix.
+
+    Parameters
+    ----------
+    linear : np.ndarray
+        condensed matrix
+    directed : bool
+        whether the condensed matrix is directed
+
+    Returns
+    -------
+    square_array : np.ndarray
+        square matrix
+    """
+    n = square_size(len(linear), directed)
+    i, j = offdiag_indices(n, directed)
+    square = np.zeros((n, n), linear.dtype)
+    square[i, j] = linear
+    if not directed:
+        square += square.T
+    return square
+
+
+def linear_array(square, directed=True):
+    """
+    Convert a square matrix to a linear vector.
+
+    Parameters
+    ----------
+    square_array : np.ndarray
+        square matrix
+    directed : bool
+        whether the condensed matrix is directed
+
+    Returns
+    -------
+    linear : np.ndarray
+        condensed matrix
+    """
+    i, j = offdiag_indices(square.shape[0], directed)
+    return square[i, j]
